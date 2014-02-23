38b626232048ea9adca2daae63ccee5475f52aa3
xbird <xbird007@gmail.com>
2014-02-23 23:31:29 +0700

    first

---
+++ Visual Micro/.CopterControl.vsarduino.h
@@ -0,0 +1,58 @@
+#ifndef _VSARDUINO_H_
+#define _VSARDUINO_H_
+//Board = Arduino Mega 2560 or Mega ADK
+#define __AVR_ATmega2560__
+#define ARDUINO 105
+#define __AVR__
+#define F_CPU 16000000L
+#define __cplusplus
+#define __attribute__(x)
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __asm__ 
+#define __volatile__
+#define __builtin_va_list
+#define __builtin_va_start
+#define __builtin_va_end
+#define __DOXYGEN__
+#define prog_void
+#define PGM_VOID_P int
+#define NOINLINE __attribute__((noinline))
+
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {;}
+
+
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\mega\pins_arduino.h" 
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\CopterControl.ino"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Alarms.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Alarms.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\EEPROM.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\EEPROM.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\GPS.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\GPS.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\IMU.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\IMU.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\LCD.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\LCD.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\MultiWii.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\MultiWii.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Output.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Output.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Protocol.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Protocol.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\RX.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\RX.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Sensors.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Sensors.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Serial.cpp"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\Serial.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\config.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\def.h"
+#include "C:\Users\XB\Dropbox\Melon\CopterControl\types.h"
+#endif
